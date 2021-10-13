acaf46633fb2afb3a7e7a115c5996410e8eb6919
Kevin Ardila <kevinardila2001@gmail.com>
2021-10-13 01:41:19 -0500

    SQL folder

---
+++ APItimenews/APItimenews/Models/Historial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APItimenews.Models
+{
+    public class Historial
+    {
+        public int HistorialName { get; set; }
+    }
+}
